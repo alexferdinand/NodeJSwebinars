a81996fd3d5f0efc305a3115969f5d4d429c34f1
Alexander Fomin <vrabotu@gmail.com>
2020-02-11 11:36:59 +0400

    Промежуточные итоги по работе 6

---
+++ models/task.js
@@ -0,0 +1,116 @@
+const mysql = require('mysql');
+const config = require('./config.json');
+
+const pool = mysql.createPool(config);
+
+module.exports = function () {
+    this.getAll = function () {
+        return new Promise(((resolve, reject) => {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    connection.release();
+                    reject(err);
+                }
+                connection.query('SELECT * FROM `tasks`', (err, rows) => {
+                    if (err) {
+                        connection.release();
+                        reject(err);
+                    }
+                    console.log(rows)
+                    const clearRows = JSON.parse(JSON.stringify(rows));
+                    // const clearRows = {...rows};
+
+                    connection.release();
+                    resolve(clearRows);
+                });
+            });
+        }));
+    }
+
+    this.getById = function (id) {
+        return new Promise(((resolve, reject) => {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    connection.release();
+                    reject(err);
+                }
+                connection.query('SELECT * FROM `tasks` WHERE tasks_id = ?', id, (err, rows) => {
+                    if (err) {
+                        connection.release();
+                        reject(err);
+                    }
+                    const clearRows = JSON.parse(JSON.stringify(rows));
+                    // const clearRows = {...rows};
+
+                    connection.release();
+                    resolve(clearRows);
+                });
+            });
+        }));
+    }
+
+    this.updateById = function (id, task) {
+        return new Promise(((resolve, reject) => {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    connection.release();
+                    reject(err);
+                }
+                connection.query('UPDATE `tasks` SET task = ?  WHERE tasks_id = ?', [task.task, id], (err, result) => {
+                    if (err) {
+                        connection.release();
+                        reject(err);
+                    }
+
+                    connection.release();
+                    resolve(result);
+                });
+            });
+        }));
+    }
+
+    this.add = function (task) {
+        return new Promise(((resolve, reject) => {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    connection.release();
+                    reject(err);
+                }
+                if (task.task !== '') {
+                connection.query('INSERT INTO `tasks` (task, users_id) VALUES (?, ?)', [task.task, task.users_id], (err, result) => {
+                    if (err) {
+                        connection.release();
+                        reject(err);
+                    }
+
+                    connection.release();
+                    resolve(result.insertId);
+                }); } else {
+                    reject("Пустое значение")
+                }
+            });
+        }));
+    }
+
+    this.deleteById = function (id) {
+        return new Promise(((resolve, reject) => {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    connection.release();
+                    reject(err);
+                }
+
+                connection.query('DELETE FROM `tasks` WHERE tasks_id = ?', id, (err, result) => {
+                    if (err) {
+                        connection.release();
+                        reject(err);
+                    }
+
+                    connection.release();
+                    console.log(result)
+                    resolve();
+                });
+            });
+        }));
+    }
+}
---
+++ routes/tasks.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router()
+const tasks = require('../models/task')
+const Task = new tasks
+
+router.get('/tasks', (req, res) => {
+    Task.getAll().then(result => {
+        console.log(result)
+            res.render('tasks', result)
+        },
+        error => {
+            console.log(error)
+        }
+    )
+});
+
+router.get('/tasks/:id',  (req, res) => {
+    console.log(req.params)
+    Task.getById(req.params.id).then(result => {
+        res.json(result)
+    },
+    error => {
+        console.log(error)
+    }
+)
+})
+
+
+router.post('/tasks',  (req, res) => {
+    console.log(req.body)
+    switch (req.body._method)  {
+       case "DELETE" :
+            Task.deleteById(req.body.tasks_id).then(result => {
+                res.redirect('/tasks')
+        },
+        error => {
+            console.log(error)
+        })
+        break
+
+        case "PUT" :
+            console.log (req.body)
+            Task.updateById(req.body.tasks_id, req.body).then(result => {
+                res.json(`Обновлено записей ${result.affectedRows}`)
+            },
+            error => {
+                console.log(error)
+            } 
+            )
+            break
+
+        default: 
+        Task.add(req.body).then(result => {
+            res.redirect('/tasks')
+        },
+        error => {
+            console.log(error)
+            res.redirect('/tasks')
+        } 
+        ) 
+        break
+}
+})
+
+
+module.exports = router
